832b4de141152786cd194b5000c7efae7a8986fb
Burgas <burgassme@gmail.com>
2025-04-06 15:26:44 +0700

    Adding identity image service

--- ticket-service/src/main/java/org/burgas/ticketservice/exception/FileIsEmptyException.java
+++
@@ -1,8 +0,0 @@
-package org.burgas.ticketservice.exception;
-
-public class FileIsEmptyException extends RuntimeException {
-
-    public FileIsEmptyException(String message) {
-        super(message);
-    }
-}
--- ticket-service/src/main/java/org/burgas/ticketservice/exception/WrongFileFormatException.java
+++
@@ -1,8 +0,0 @@
-package org.burgas.ticketservice.exception;
-
-public class WrongFileFormatException extends RuntimeException {
-
-    public WrongFileFormatException(String message) {
-        super(message);
-    }
-}
