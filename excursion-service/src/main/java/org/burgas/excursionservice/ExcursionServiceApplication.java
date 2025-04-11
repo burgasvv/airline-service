ed02a99ef8e95ca597ef635c1ad11451d1972674
Burgas <burgassme@gmail.com>
2025-04-11 20:16:19 +0700

    Adding new module service excursion-service

---
+++ excursion-service/src/main/java/org/burgas/excursionservice/ExcursionServiceApplication.java
@@ -0,0 +1,13 @@
+package org.burgas.excursionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExcursionServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExcursionServiceApplication.class, args);
+    }
+
+}
---
+++ excursion-service/src/test/java/org/burgas/excursionservice/ExcursionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.burgas.excursionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExcursionServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
