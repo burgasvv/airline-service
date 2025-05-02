9ad62f9393ffa919ec26a497ba84f279f26a9e56
Burgas <burgassme@gmail.com>
2025-05-02 14:38:43 +0700

    Adding tests for Excursion Router

--- excursion-backend/src/test/java/org/burgas/excursionbackend/service/AuthorityServiceTest.java
+++ excursion-backend/src/test/java/org/burgas/excursionbackend/service/AuthorityServiceTest.java
@@ -7,7 +7,6 @@ import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureDataJpa;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.test.context.support.WithMockUser;
 
 import java.util.List;
 
