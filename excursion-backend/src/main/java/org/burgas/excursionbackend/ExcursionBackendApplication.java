3e5813340a7226722d68740db7d1f0021c1ab3fe
Burgas <burgassme@gmail.com>
2025-05-21 12:10:53 +0700

    Adding AsyncConfig and BCryptConfig

--- excursion-backend/src/main/java/org/burgas/excursionbackend/ExcursionBackendApplication.java
+++ excursion-backend/src/main/java/org/burgas/excursionbackend/ExcursionBackendApplication.java
@@ -2,15 +2,8 @@ package org.burgas.excursionbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.core.task.AsyncTaskExecutor;
-import org.springframework.core.task.support.TaskExecutorAdapter;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-
-import java.util.concurrent.Executors;
 
 @EnableAsync
 @EnableScheduling
